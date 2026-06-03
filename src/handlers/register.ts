321526beab34f370d9dd90171c7a8eaa3b1ebaa0
Joshua Wertheim <joshua.wertheim@descope.com>
2026-06-03 13:53:33 -0700

    Revert "rename client secret -> cleartext"
    This reverts commit abe364dec4008ddbadab6ed5f4d8201a1eaba995.

--- src/handlers/register.ts
+++ src/handlers/register.ts
@@ -133,7 +133,7 @@ async function registerClient(
   const createAppResponseJson =
     (await createAppResponse.json()) as CreateAppResponse;
   const appId = createAppResponseJson.id;
-  const cleartext = createAppResponseJson.cleartext || undefined;
+  const client_secret = createAppResponseJson.cleartext || undefined;
 
   const loadAppResponse = await fetch(
     `${provider.baseUrl}/v1/mgmt/thirdparty/app/load?id=${appId}`,
@@ -163,7 +163,7 @@ async function registerClient(
 
   return OAuthClientInformationFullSchema.parse({
     client_id,
-    cleartext,
+    client_secret,
     ...client,
   });
 }
